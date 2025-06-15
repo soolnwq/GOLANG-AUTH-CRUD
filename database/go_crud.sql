023934b228e94a02852022bdcd05cf48ef9904a7
Suthep Phomnoi <kiwkub04@gmail.com>
2025-06-15 13:50:37 +0700

    chore: update users table field password_hash to password

--- database/go_crud.sql
+++ database/go_crud.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
+    password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
